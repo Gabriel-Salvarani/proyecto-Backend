e35255abd87564387a7267a24e6b46e36d0b04e8
Gabriel-Salvarani <gabrielsalvarani@gmail.com>
2025-12-04 16:19:44 -0300

    add: conexion con mongodb

---
+++ src/config/mongodb.ts
@@ -0,0 +1,15 @@
+import { connect } from "mongoose"
+import process = require("process")
+
+const connectDB = async () => {
+  const URI_DB = process.env.URI_DB!
+  try {
+    await connect(URI_DB)
+    console.log("✅ Conectado a Mongo DB con éxito!")
+  } catch (e) {
+    console.log("❌ Error al conectarse a Mongo DB")
+    process.exit(1)
+  }
+}
+
+export default connectDB
